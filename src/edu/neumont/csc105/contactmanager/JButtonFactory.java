9c2bfcb3f4c70cccc5b7d258bd311e9c44b9a081
Brian Mondido <bmondido@student.neumont.edu>
2012-08-22 21:33:35 -0600

    created JButtonFactory

--- src/edu/neumont/csc105/contactmanager/JButtonFactory.java
+++ src/edu/neumont/csc105/contactmanager/JButtonFactory.java
@@ -1,5 +1,12 @@
 package edu.neumont.csc105.contactmanager;
 
+import java.util.Map;
+
+import javax.swing.JButton;
+
 public class JButtonFactory {
 
+	private Map<String,JButton> buttons;
+	
+	
 }
